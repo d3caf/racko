3e46596dad6ea6e288e27372fe738cc4efcd207d
Andrew Anguiano <andrew.anguiano@amobee.com>
2020-05-19 11:48:09 -0400

    remove lobby stuff

--- lib/racko.ex
+++ lib/racko.ex
@@ -7,13 +7,10 @@ defmodule Racko do
   def start(_type, _args) do
     children = [
       {Registry, keys: :unique, name: Racko.GameRegistry},
-      {Registry, keys: :unique, name: Racko.LobbyRegistry},
-      Racko.GameSupervisor,
-      Racko.LobbySupervisor
+      Racko.GameSupervisor
     ]
 
     :ets.new(:games_table, [:public, :named_table])
-    :ets.new(:lobbies_table, [:public, :named_table])
 
     opts = [strategy: :one_for_one, name: Racko.Supervisor]
     Supervisor.start_link(children, opts)
